ae6d32d125239e8e6f3738bc0b47edbf3252fff4
Rahul <rahul.panwar900.com>
2022-08-29 21:50:57 +0530

    search bar added

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import AppBar from "@mui/material/AppBar";
-import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
-import Typography from "@mui/material/Typography";
 import MenuIcon from "@mui/icons-material/Menu";
+import {
+  Typography,
+  Box,
+  makeStyles,
+} from "@material-ui/core";
+import { borderRadius, padding } from "@mui/system";
+
+const useStyles =makeStyles(theme=>({
+search:{
+position:"absolute",
+right:10,
+height:30,
+borderRadius:10,
+padding:5,
+fontSize:18,
+[theme.breakpoints.down('sm')]:{
+  width:200
+}
+},
+title:{
+  [theme.breakpoints.down('sm')]:{
+    position:"absolute",
+    left:0,
+    fontSize:22
+}
+
+
+}}))
 
 const Header = () => {
+const classes=useStyles()
+const [query,setQuery]=useState("")
 
+const changeHandler=(e)=>{
+setQuery(e.target.value)
+}
   return (
     <div>
       <Box sx={{ flexGrow: 1 }}>
@@ -26,11 +57,12 @@ const Header = () => {
               variant="h4"
               noWrap
               component="div"
+              className={classes.title}
             >
               Country App
             </Typography>
-
-            </Toolbar>
+          <input type="text" placeholder="serach for country here" className={classes.search} onChange={changeHandler}/>
+          </Toolbar>
         </AppBar>
       </Box>
     </div>
